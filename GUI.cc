dc2fd89078c50e14501e777d473b764633b43b86
saaxxon <115244726+saaxxon@users.noreply.github.com>
2023-04-16 12:27:15 -0600

    Create GUI.cc

---
+++ GUI.cc
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// Define the Item structure
+struct Item {
+    string name;
+    string content;
+    int difficulty;
+    bool reviewed;
+    int review_interval;
+};
+
+// Define the ItemList class
+class ItemList {
+public:
+    void addItem(Item item);
+    void deleteItem(int index);
+    void editItem(int index, Item item);
+    void listItems();
+    Item getItem(int index);
+private:
+    vector<Item> items;
+};
+
+// Method to add an item to the list
+void ItemList::addItem(Item item) {
+    items.push_back(item);
+}
+
+// Method to delete an item from the list
+void ItemList::deleteItem(int index) {
+    items.erase(items.begin() + index);
+}
+
+// Method to edit an existing item
+void ItemList::editItem(int index, Item item) {
+    items[index] = item;
+}
+
+// Method to list all items in the list
+void ItemList::listItems() {
+    for (int i = 0; i < items.size(); i++) {
+        cout << i+1 << ". " << items[i].name << endl;
+    }
+}
+
+// Method to get an item from the list
+Item ItemList::getItem(int index) {
+    return items[index];
+}
+
+// Define the UserInterface class
+class UserInterface {
+public:
+    void mainMenu();
+    void addItem();
+    void deleteItem();
+    void editItem();
+    void listItems();
+    void reviewMode();
+private:
+    ItemList itemList;
+};
+
+// Method to display the main menu
+void UserInterface::mainMenu() {
+    cout << "Main Menu:" << endl;
+    cout << "1. Add Item" << endl;
+    cout << "2. Delete Item" << endl;
+    cout << "3. Edit Item" << endl;
+    cout << "4. List Items" << endl;
+    cout << "5. Review Mode" << endl;
+    cout << "6. Exit" << endl;
+}
+
+// Method to add an item to the list
+void UserInterface::addItem() {
+    Item item;
+    cout << "Enter item name: ";
+    getline(cin, item.name);
+    cout << "Enter item content: ";
+    getline(cin, item.content);
+    cout << "Enter item difficulty (1-5): ";
+    cin >> item.difficulty;
+    item.reviewed = false;
+    item.review_interval = 1;
+    cin.ignore();
+    itemList.addItem(item);
+}
+
+// Method to delete an item from the list
+void UserInterface::deleteItem() {
+    int index;
+    cout << "Enter the index of the item to delete: ";
+    cin >> index;
+    cin.ignore();
+    itemList.deleteItem(index - 1);
+}
+
+// Method to edit an existing item
+void UserInterface::editItem() {
+    int index;
+    Item item;
+    cout << "Enter the index of the item to edit: ";
+    cin >> index;
+    cin.ignore();
+    item = itemList.getItem(index - 1);
+    cout << "Enter the new item name: ";
+    getline(cin, item.name);
+    cout << "Enter the new item content: ";
+    getline(cin, item.content);
+    cout << "Enter the new item difficulty (1-5): ";
+    cin >> item.difficulty;
+    cin.ignore();
+    itemList.editItem(index - 1, item);
+}
+
+// Method to list all items in the list
+void UserInterface::listItems() {
+    itemList.listItems();
+}
+
+// Method to enter review mode
+void UserInterface::reviewMode() {
+    cout << "Entering review mode..." <<
