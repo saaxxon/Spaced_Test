00fd3399b6f7b61ba482fc680642656b4012d890
saaxxon <115244726+saaxxon@users.noreply.github.com>
2023-04-16 13:34:24 -0600

    Update Algorithm.cc

--- Algorithm.cc
+++ Algorithm.cc
@@ -1,94 +1,133 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
 #include <ctime>
 #include <cmath>
+
 using namespace std;
 
-struct Card {
-    int id;
+// Define a struct to represent a flashcard
+struct Flashcard {
+    string question;
+    string answer;
+    int difficulty;
     int interval;
-    int repetitions;
-    time_t next_review;
 };
 
-class SpacedRepetition {
-private:
-    double ease_factor;
-    double minimum_interval;
-    double initial_interval;
-    double forgetting_index;
-public:
-    SpacedRepetition(double ease_factor, double minimum_interval, double initial_interval, double forgetting_index) {
-        this->ease_factor = ease_factor;
-        this->minimum_interval = minimum_interval;
-        this->initial_interval = initial_interval;
-        this->forgetting_index = forgetting_index;
+// Define a function to calculate the interval between repetitions
+int calculateInterval(int difficulty, int interval, double responseTime, bool correctAnswer) {
+    double easinessFactor = 2.5 - (difficulty - 1) * 0.2;
+    if (!correctAnswer) {
+        easinessFactor -= 0.8;
     }
-    double get_ease_factor() {
-        return this->ease_factor;
+    if (easinessFactor < 1.3) {
+        easinessFactor = 1.3;
     }
-    double get_minimum_interval() {
-        return this->minimum_interval;
+    interval = ceil(interval * easinessFactor);
+    if (interval < 1) {
+        interval = 1;
     }
-    double get_initial_interval() {
-        return this->initial_interval;
+    return interval;
+}
+
+// Define a function to read in flashcards from a file
+Flashcard* readFlashcards(string filename, int& numCards) {
+    ifstream infile(filename.c_str());
+    if (!infile) {
+        cerr << "Error: Could not open file '" << filename << "' for reading." << endl;
+        exit(1);
     }
-    double get_forgetting_index() {
-        return this->forgetting_index;
-    }
-    double calculate_easiness_factor(Card card) {
-        double easiness_factor = card.interval / this->initial_interval;
-        easiness_factor = easiness_factor - 0.8 + 0.28 * card.repetitions - 0.02 * card.repetitions * card.repetitions;
-        if (easiness_factor < 1.3) {
-            easiness_factor = 1.3;
+    vector<Flashcard> flashcards;
+    string line;
+    while (getline(infile, line)) {
+        istringstream iss(line);
+        string question, answer;
+        if (!(iss >> question >> answer)) {
+            cerr << "Error: Invalid input line: " << line << endl;
+            exit(1);
         }
-        return easiness_factor;
+        flashcards.push_back({question, answer, 1, 1});
     }
-    int calculate_next_interval(Card card, double easiness_factor) {
-        int next_interval = (int)round(card.interval * easiness_factor);
-        if (next_interval < this->minimum_interval) {
-            next_interval = (int)this->minimum_interval;
-        }
-        return next_interval;
+    numCards = flashcards.size();
+    Flashcard* cards = new Flashcard[numCards];
+    copy(flashcards.begin(), flashcards.end(), cards);
+    return cards;
+}
+
+// Define a function to write flashcards to a file
+void writeFlashcards(string filename, Flashcard* cards, int numCards) {
+    ofstream outfile(filename.c_str());
+    if (!outfile) {
+        cerr << "Error: Could not open file '" << filename << "' for writing." << endl;
+        exit(1);
     }
-    int calculate_repetitions(Card card, double easiness_factor) {
-        int repetitions = card.repetitions;
-        if (easiness_factor <= 1.3) {
-            repetitions = 0;
-        }
-        else {
-            repetitions++;
-        }
-        return repetitions;
+    for (int i = 0; i < numCards; i++) {
+        outfile << cards[i].question << " " << cards[i].answer << endl;
     }
-    Card update_card(Card card, double easiness_factor) {
-        time_t current_time = time(nullptr);
-        int repetitions = calculate_repetitions(card, easiness_factor);
-        int interval = calculate_next_interval(card, easiness_factor);
-        card.repetitions = repetitions;
-        card.interval = interval;
-        card.next_review = current_time + interval * 86400;
-        return card;
+    outfile.close();
+}
+
+// Define a function to print out the list of flashcards
+void printFlashcards(Flashcard* cards, int numCards) {
+    for (int i = 0; i < numCards; i++) {
+        cout << cards[i].question << " " << cards[i].answer << " " << cards[i].difficulty << " " << cards[i].interval << endl;
     }
-};
+}
 
-int main() {
-    // initialize the spaced repetition algorithm
-    SpacedRepetition sr(2.5, 1, 10, 0.16);
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cerr << "Usage: " << argv[0] << " <flashcard-file>" << endl;
+        exit(1);
+    }
+    string filename = argv[1];
+    int numCards;
+    Flashcard* flashcards = readFlashcards(filename, numCards);
+    int currentCard = 0;
+    int numCorrect = 0;
+    int numIncorrect = 0;
+    double totalTime = 0.0;
 
-    // create a new card
-    Card card = { 1, 1, 0, time(nullptr) };
+    srand(time(nullptr));
 
-    // calculate the easiness factor for the card
-    double easiness_factor = sr.calculate_easiness_factor(card);
+    while (true) {
+        Flashcard& card = flashcards[currentCard];
+        cout << card.question << endl;
+        string answer;
+        cin >> answer;
+        double responseTime = difftime(time(nullptr), mktime(localtime(&t)));
+        bool correctAnswer =
+    if (answer == card.answer) {
+        cout << "Correct!" << endl;
+        card.difficulty++;
+        numCorrect++;
+        card.interval = calculateInterval(card.difficulty, card.interval, responseTime, true);
+    } else {
+        cout << "Incorrect. The correct answer is: " << card.answer << endl;
+        card.difficulty = 1;
+        numIncorrect++;
+        card.interval = calculateInterval(card.difficulty, card.interval, responseTime, false);
+    }
+    totalTime += responseTime;
+    cout << "Time taken: " << responseTime << " seconds" << endl;
+    currentCard++;
+    if (currentCard == numCards) {
+        currentCard = 0;
+        random_shuffle(&flashcards[0], &flashcards[numCards]);
+    }
+    cout << "Number of correct answers: " << numCorrect << endl;
+    cout << "Number of incorrect answers: " << numIncorrect << endl;
+    cout << "Total time taken: " << totalTime << " seconds" << endl;
+    writeFlashcards(filename, flashcards, numCards);
 
-    // update the card with the new easiness factor
-    card = sr.update_card(card, easiness_factor);
-
-    // print out the results
-    cout << "Card id: " << card.id << endl;
-    cout << "Repetitions: " << card.repetitions << endl;
-    cout << "Interval: " << card.interval << " days" << endl;
-    cout << "Next review: " << ctime(&card.next_review) << endl;
-
-    return 0;
+    // check if the user wants to continue reviewing
+    char continueReviewing;
+    cout << "Do you want to continue reviewing? (y/n) ";
+    cin >> continueReviewing;
+    if (continueReviewing == 'n') {
+        break;
+    }
+}
+delete[] flashcards;
+return 0;
 }
