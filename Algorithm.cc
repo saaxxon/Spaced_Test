f856c0916cfa5aec5d6801bd2f838785fb0d87ed
saaxxon <115244726+saaxxon@users.noreply.github.com>
2023-04-16 17:21:41 -0600

    Update Algorithm.cc

--- Algorithm.cc
+++ Algorithm.cc
@@ -1,77 +1,47 @@
 #include "Algorithm.h"
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <vector>
 
-double calculateInterval(double difficulty, double currentInterval, int responseTime, bool correct) {
-    double nextInterval = 0;
-    if (responseTime > 0 && correct) {
-        if (currentInterval == 0) {
-            nextInterval = 1;
-        } else if (currentInterval == 1) {
-            nextInterval = 6;
-        } else {
-            nextInterval = currentInterval * difficulty;
-        }
-    } else {
-        nextInterval = 0;
-    }
-    return nextInterval;
+// Initializes the SuperMemo algorithm
+void SuperMemo::init(Card& card) {
+  card.easinessFactor = 2.5;
+  card.repetitions = 0;
+  card.interval = 1;
 }
 
-Flashcard* readFlashcards(const std::string& filename) {
-    std::fstream file(filename);
-    if (!file.is_open()) {
-        std::cout << "Error: Could not open file " << filename << std::endl;
-        return nullptr;
-    }
-    if (file.peek() == std::ifstream::traits_type::eof()) {
-        std::cout << "Error: File " << filename << " is empty" << std::endl;
-        return nullptr;
-    }
-    std::vector<Flashcard> flashcards;
-    std::string line;
-    while (std::getline(file, line)) {
-        std::string::size_type sep = line.find(",");
-        if (sep == std::string::npos) {
-            std::cout << "Error: Invalid line in file " << filename << std::endl;
-            return nullptr;
-        }
-        std::string front = line.substr(0, sep);
-        std::string back = line.substr(sep + 1);
-        Flashcard flashcard(front, back);
-        flashcards.push_back(flashcard);
-    }
-    Flashcard* flashcardPtr = new Flashcard[flashcards.size()];
-    for (int i = 0; i < flashcards.size(); i++) {
-        flashcardPtr[i] = flashcards[i];
-    }
-    return flashcardPtr;
+// Calculates the next interval and easiness factor for a card
+void SuperMemo::calculate(Card& card, int responseQuality) {
+  float easinessFactor = card.easinessFactor;
+  int repetitions = card.repetitions;
+  int interval = card.interval;
+
+  if (responseQuality < 3) {
+    repetitions = 0;
+    interval = 1;
+  } else {
+    easinessFactor = calculateEasinessFactor(easinessFactor, responseQuality);
+    repetitions++;
+    interval = calculateInterval(repetitions, easinessFactor);
+  }
+
+  card.easinessFactor = easinessFactor;
+  card.repetitions = repetitions;
+  card.interval = interval;
 }
 
-void writeFlashcards(const std::string& filename, const Flashcard* flashcards, int numFlashcards) {
-    std::ofstream file(filename);
-    if (!file.is_open()) {
-        std::cout << "Error: Could not open file " << filename << std::endl;
-        return;
-    }
-    for (int i = 0; i < numFlashcards; i++) {
-        file << flashcards[i].getFront() << "," << flashcards[i].getBack() << std::endl;
-    }
+// Calculates the new easiness factor for a card
+float SuperMemo::calculateEasinessFactor(float easinessFactor, int responseQuality) {
+  float newEasinessFactor = easinessFactor - 0.8 + 0.28 * responseQuality - 0.02 * responseQuality * responseQuality;
+  newEasinessFactor = std::max(newEasinessFactor, 1.3f);
+  return newEasinessFactor;
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        std::cout << "Usage: " << argv[0] << " <filename>" << std::endl;
-        return 1;
-    }
-    std::string filename = argv[1];
-    Flashcard* flashcardPtr = readFlashcards(filename);
-    if (flashcardPtr == nullptr) {
-        return 1;
-    }
-    // Do something with flashcards...
-    delete[] flashcardPtr;
-    return 0;
+// Calculates the new interval for a card
+int SuperMemo::calculateInterval(int repetitions, float easinessFactor) {
+  if (repetitions == 1) {
+    return 1;
+  } else if (repetitions == 2) {
+    return 6;
+  } else {
+    int interval = ceil(easinessFactor * calculateInterval(repetitions - 1, easinessFactor));
+    return interval;
+  }
 }
