3a1768fd4bd81d27209312fade678dc6e0573fee
saaxxon <115244726+saaxxon@users.noreply.github.com>
2023-04-16 17:25:52 -0600

    Update Integration.cc

--- Integration.cc
+++ Integration.cc
@@ -1,23 +1,94 @@
-// Integration.cc
+#include "Integration.hh"
 
-#include "Integration.h"
-#include "GUI.h"
-#include "Algorithm.h"
+#include <fstream>
 
-void integrate_algorithm_with_gui(GUI& gui) {
-  // Connect the button clicked signal to the corresponding callback function
-  gui.connect_button_clicked_signal([&gui](){
-    // Parse the user input
-    std::string input = gui.get_input();
-    InputData data = parse_input(input);
-    
-    // Create the review schedule
-    std::vector<ReviewItem> schedule = create_review_schedule(data);
-    
-    // Format the schedule as a string
-    std::string schedule_string = format_schedule(schedule);
-    
-    // Update the GUI with the review schedule
-    gui.update_schedule(schedule_string);
-  });
+// Saves the deck to a file
+void Integration::saveDeck(const Deck& deck, const std::string& filename) {
+  std::ofstream outfile(filename);
+
+  if (outfile.is_open()) {
+    // Write deck name
+    outfile << deck.name << "\n";
+
+    // Write cards
+    for (const auto& card : deck.cards) {
+      outfile << card.front << "\n";
+      outfile << card.back << "\n";
+      outfile << card.interval << "\n";
+      outfile << card.easiness << "\n";
+      outfile << card.lastReviewed << "\n";
+      outfile << card.nextReview << "\n";
+      outfile << card.repetitions << "\n";
+      outfile << card.quality << "\n";
+      outfile << card.history << "\n";
+    }
+
+    outfile.close();
+  }
+}
+
+// Loads the deck from a file
+Deck Integration::loadDeck(const std::string& filename) {
+  std::ifstream infile(filename);
+  Deck deck;
+
+  if (infile.is_open()) {
+    std::string line;
+    bool firstLine = true;
+    Card card;
+
+    while (std::getline(infile, line)) {
+      if (line.empty()) continue;
+
+      if (firstLine) {
+        deck.name = line;
+        firstLine = false;
+      } else {
+        switch (card.field) {
+          case CardField::FRONT:
+            card.front = line;
+            card.field = CardField::BACK;
+            break;
+          case CardField::BACK:
+            card.back = line;
+            card.field = CardField::INTERVAL;
+            break;
+          case CardField::INTERVAL:
+            card.interval = std::stoi(line);
+            card.field = CardField::EASINESS;
+            break;
+          case CardField::EASINESS:
+            card.easiness = std::stof(line);
+            card.field = CardField::LAST_REVIEWED;
+            break;
+          case CardField::LAST_REVIEWED:
+            card.lastReviewed = std::stol(line);
+            card.field = CardField::NEXT_REVIEW;
+            break;
+          case CardField::NEXT_REVIEW:
+            card.nextReview = std::stol(line);
+            card.field = CardField::REPETITIONS;
+            break;
+          case CardField::REPETITIONS:
+            card.repetitions = std::stoi(line);
+            card.field = CardField::QUALITY;
+            break;
+          case CardField::QUALITY:
+            card.quality = std::stoi(line);
+            card.field = CardField::HISTORY;
+            break;
+          case CardField::HISTORY:
+            card.history = std::vector<int>(std::stoi(line));
+            card.field = CardField::FRONT;
+            deck.cards.push_back(card);
+            card = Card();
+            break;
+        }
+      }
+    }
+
+    infile.close();
+  }
+
+  return deck;
 }
