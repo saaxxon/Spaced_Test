f898d7d2536741aca29d28c89b676c770f20f003
saaxxon <115244726+saaxxon@users.noreply.github.com>
2023-04-16 13:36:59 -0600

    Update Algorithm.cc

--- Algorithm.cc
+++ Algorithm.cc
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
-#include <cstdlib>
-#include <ctime>
-#include <cmath>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <random>
 
 using namespace std;
 
-// Define a struct to represent a flashcard
 struct Flashcard {
     string question;
     string answer;
@@ -15,119 +14,116 @@ struct Flashcard {
     int interval;
 };
 
-// Define a function to calculate the interval between repetitions
-int calculateInterval(int difficulty, int interval, double responseTime, bool correctAnswer) {
-    double easinessFactor = 2.5 - (difficulty - 1) * 0.2;
-    if (!correctAnswer) {
-        easinessFactor -= 0.8;
+vector<Flashcard> readFlashcards(string filename) {
+    vector<Flashcard> flashcards;
+    ifstream file(filename);
+    if (file.is_open()) {
+        string line;
+        while (getline(file, line)) {
+            Flashcard card;
+            card.question = line;
+            getline(file, card.answer);
+            file >> card.difficulty;
+            file >> card.interval;
+            flashcards.push_back(card);
+            file.ignore(); // ignore the blank line
+        }
+        file.close();
     }
-    if (easinessFactor < 1.3) {
-        easinessFactor = 1.3;
+    return flashcards;
+}
+
+void writeFlashcards(string filename, Flashcard* flashcards, int numCards) {
+    ofstream file(filename);
+    if (file.is_open()) {
+        for (int i = 0; i < numCards; i++) {
+            file << flashcards[i].question << endl;
+            file << flashcards[i].answer << endl;
+            file << flashcards[i].difficulty << endl;
+            file << flashcards[i].interval << endl;
+            if (i != numCards - 1) {
+                file << endl;
+            }
+        }
+        file.close();
     }
-    interval = ceil(interval * easinessFactor);
-    if (interval < 1) {
+}
+
+int calculateInterval(int difficulty, int interval, int responseTime, bool answeredCorrectly) {
+    double easinessFactor = 0.1 - (difficulty - 1) * (0.08 + (difficulty - 1) * 0.02);
+    if (answeredCorrectly) {
         interval = 1;
+    } else if (difficulty > 1) {
+        interval *= easinessFactor;
     }
+    interval = round(interval);
     return interval;
 }
 
-// Define a function to read in flashcards from a file
-Flashcard* readFlashcards(string filename, int& numCards) {
-    ifstream infile(filename.c_str());
-    if (!infile) {
-        cerr << "Error: Could not open file '" << filename << "' for reading." << endl;
-        exit(1);
-    }
-    vector<Flashcard> flashcards;
-    string line;
-    while (getline(infile, line)) {
-        istringstream iss(line);
-        string question, answer;
-        if (!(iss >> question >> answer)) {
-            cerr << "Error: Invalid input line: " << line << endl;
-            exit(1);
-        }
-        flashcards.push_back({question, answer, 1, 1});
-    }
-    numCards = flashcards.size();
-    Flashcard* cards = new Flashcard[numCards];
-    copy(flashcards.begin(), flashcards.end(), cards);
-    return cards;
-}
-
-// Define a function to write flashcards to a file
-void writeFlashcards(string filename, Flashcard* cards, int numCards) {
-    ofstream outfile(filename.c_str());
-    if (!outfile) {
-        cerr << "Error: Could not open file '" << filename << "' for writing." << endl;
-        exit(1);
-    }
-    for (int i = 0; i < numCards; i++) {
-        outfile << cards[i].question << " " << cards[i].answer << endl;
-    }
-    outfile.close();
-}
-
-// Define a function to print out the list of flashcards
-void printFlashcards(Flashcard* cards, int numCards) {
-    for (int i = 0; i < numCards; i++) {
-        cout << cards[i].question << " " << cards[i].answer << " " << cards[i].difficulty << " " << cards[i].interval << endl;
-    }
-}
-
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        cerr << "Usage: " << argv[0] << " <flashcard-file>" << endl;
-        exit(1);
-    }
-    string filename = argv[1];
-    int numCards;
-    Flashcard* flashcards = readFlashcards(filename, numCards);
+int main() {
+    string filename = "flashcards.txt";
+    vector<Flashcard> flashcards = readFlashcards(filename);
+    int numCards = flashcards.size();
+    Flashcard* flashcardPtr = &flashcards[0];
+    random_device rd;
+    mt19937 g(rd());
+    shuffle(flashcards.begin(), flashcards.end(), g);
     int currentCard = 0;
     int numCorrect = 0;
     int numIncorrect = 0;
-    double totalTime = 0.0;
-
-    srand(time(nullptr));
-
+    int totalTime = 0;
     while (true) {
-        Flashcard& card = flashcards[currentCard];
-        cout << card.question << endl;
-        string answer;
-        cin >> answer;
-        double responseTime = difftime(time(nullptr), mktime(localtime(&t)));
-        bool correctAnswer =
-    if (answer == card.answer) {
+        Flashcard& card = flashcardPtr[currentCard];
+        cout << card
+                cout << "Question: " << card.question << endl;
+    string userInput;
+    cout << "Enter your answer: ";
+    getline(cin, userInput);
+    if (userInput == card.answer) {
         cout << "Correct!" << endl;
-        card.difficulty++;
         numCorrect++;
+        int responseTime;
+        cout << "Enter your response time (in seconds): ";
+        cin >> responseTime;
+        totalTime += responseTime;
         card.interval = calculateInterval(card.difficulty, card.interval, responseTime, true);
+        currentCard++;
+        if (currentCard == numCards) {
+            cout << "You have reviewed all flashcards!" << endl;
+            break;
+        }
+        char continueReviewing;
+        cout << "Do you want to continue reviewing? (y/n): ";
+        cin >> continueReviewing;
+        if (continueReviewing == 'n') {
+            break;
+        }
     } else {
         cout << "Incorrect. The correct answer is: " << card.answer << endl;
-        card.difficulty = 1;
         numIncorrect++;
-        card.interval = calculateInterval(card.difficulty, card.interval, responseTime, false);
-    }
-    totalTime += responseTime;
-    cout << "Time taken: " << responseTime << " seconds" << endl;
-    currentCard++;
-    if (currentCard == numCards) {
-        currentCard = 0;
-        random_shuffle(&flashcards[0], &flashcards[numCards]);
-    }
-    cout << "Number of correct answers: " << numCorrect << endl;
-    cout << "Number of incorrect answers: " << numIncorrect << endl;
-    cout << "Total time taken: " << totalTime << " seconds" << endl;
-    writeFlashcards(filename, flashcards, numCards);
-
-    // check if the user wants to continue reviewing
-    char continueReviewing;
-    cout << "Do you want to continue reviewing? (y/n) ";
-    cin >> continueReviewing;
-    if (continueReviewing == 'n') {
-        break;
+        card.interval = calculateInterval(card.difficulty, card.interval, 0, false);
+        currentCard++;
+        if (currentCard == numCards) {
+            cout << "You have reviewed all flashcards!" << endl;
+            break;
+        }
+        char continueReviewing;
+        cout << "Do you want to continue reviewing? (y/n): ";
+        cin >> continueReviewing;
+        if (continueReviewing == 'n') {
+            break;
+        }
     }
 }
-delete[] flashcards;
+for (int i = 0; i < numCards; i++) {
+    Flashcard& card = flashcardPtr[i];
+    writeFlashcards(filename, flashcardPtr, numCards);
+}
+double averageTime = totalTime / numCards;
+cout << "Number of flashcards reviewed: " << numCards << endl;
+cout << "Number of correct responses: " << numCorrect << endl;
+cout << "Number of incorrect responses: " << numIncorrect << endl;
+cout << "Total time spent reviewing (in seconds): " << totalTime << endl;
+cout << "Average response time (in seconds): " << averageTime << endl;
 return 0;
 }
